0bcce73f02eac43f297f9dd1eb04feac8e5ab304
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-28 02:16:23 +0600

    added updated code

--- task1.js
+++ task1.js
@@ -1,7 +1,10 @@
 function calculateTax(income, expenses) {
     let totalIncome = income - expenses;
     let payableTax = totalIncome * 20 / 100;
+    console.log(payableTax);
     return payableTax;
 }
-let totalIncome = calculateTax(5000, 1500);
-console.log(totalIncome);
+calculateTax(10000, 3000);
+calculateTax(34000, 1753);
+calculateTax(5000, 1500);
+calculateTax(7000, 7000);
