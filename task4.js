c885da11ac77b49053ecfe176f478b608a0cec6b
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-29 00:09:27 +0600

    added new task file

---
+++ task4.js
@@ -0,0 +1,19 @@
+
+function calculateFinalScore({ name, testScore, schoolGrade, isFamily }) {
+
+    if (typeof name !== "string" || typeof testScore !== "number" || testScore >= 50 || typeof schoolGrade !== "number" || schoolGrade >= 30 || typeof isFamily !== "boolean") {
+
+        return "invalid input";
+
+    }
+    let parentProfessionScore = isFamily ? 20 : 0;
+    let totalScore = testScore + schoolGrade + parentProfessionScore;
+    return totalScore >= 80;
+}
+let studentDetails = {
+    name: 'Rajib',
+    testScore: 45,
+    schoolGrade: 25,
+    isFamily: true,
+};
+console.log(calculateFinalScore(studentDetails));
