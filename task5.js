cb42be59195205495a0f240a3bde3aa3c5669180
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-29 00:37:33 +0600

    added compelete assignment task

---
+++ task5.js
@@ -0,0 +1,14 @@
+function waitingTime(interviewTime, serialNumber) {
+    if (!Array.isArray(interviewTime) || typeof serialNumber !== "number") {
+        return "invalid input";
+    }
+    let totalTime = interviewTime.reduce((sum, time) => sum + time, 0);
+    let avgTime = Math.round(totalTime / interviewTime.length);
+    let finishedInterview = interviewTime.length;
+    let candidateLeft = serialNumber - finishedInterview;
+    let waitingTime = avgTime * (candidateLeft > 0 ? candidateLeft : 0);
+    return `${waitingTime}`;
+}
+let interviewTime = [3, 5, 7, 11, 6];
+let serialNumber = 10;
+console.log(waitingTime(interviewTime, serialNumber));
