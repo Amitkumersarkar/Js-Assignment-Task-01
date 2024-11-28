4466738cc74d75d482390ea95f5f2d5ca1c85ef8
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-11-28 23:31:32 +0600

    added new task file

---
+++ task3.js
@@ -0,0 +1,15 @@
+function checkDigitsInName(input) {
+    if (typeof input !== "string") {
+        return "invalid input";
+    }
+    let hasDigit = /\d/.test(input);
+    return hasDigit;
+}
+
+console.log(checkDigitsInName("Raj123"));
+console.log(checkDigitsInName("n9ayeem"));
+console.log(checkDigitsInName("e1mu3"));
+console.log(checkDigitsInName("Suman"));
+console.log(checkDigitsInName("Name2024"));
+console.log(checkDigitsInName("!@#"));
+console.log(checkDigitsInName(["Raj"]));
